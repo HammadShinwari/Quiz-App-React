d850d6a14d45a4d58985e4a36f9b3eeda8ca6cca
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-03-20 22:44:31 +0500

    Quiz App React

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Quiz.css';
+
+function Quiz() {
+  return (
+    <div className="container">
+        <h1>Quiz App</h1>
+    </div>
+  )
+}
+
+export default Quiz;
