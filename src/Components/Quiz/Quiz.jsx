949a59cd6c4b87446fdb13fd25e2dce2a4d78a6c
Hammad Shinwari <hammadshinwari377@gmail.com>
2024-03-20 23:27:19 +0500

    Quiz App React

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -5,6 +5,16 @@ function Quiz() {
   return (
     <div className="container">
         <h1>Quiz App</h1>
+        <hr />
+        <h2>1. How many objects in javascript?</h2>
+        <ul>
+            <li>Seven</li>
+            <li>Eight</li>
+            <li>Six</li>
+            <li>Ten</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 question</div>
     </div>
   )
 }
